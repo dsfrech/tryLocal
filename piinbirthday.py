c209a8fc892e6dc30245bd71b6a49d9df94eb51d
David S Frech <dfrech@hotmail.com>
2022-06-16 14:36:04 -0400

    find MMDD in million digit pi

--- piinbirthday.py
+++ piinbirthday.py
@@ -4,12 +4,25 @@
 """
 
 FILENAME = 'data/pi_million_digits.txt'
+BIRTHDAY = '0928'                           # MMDD
 
 """
 Note that with will cause the file to be closed at the end of the block
 """
-p = open('data/pi_digits.txt')
-d = p.readline().lstrip().rstrip()
-e = p.readline().lstrip().rstrip()
-f = d + e
-print(f)
+
+with open(FILENAME, encoding='utf-8') as p:
+    data = p.readlines()
+
+pi_million = ''
+for line in data:
+    current_line = line.lstrip().rstrip()
+    pi_million += current_line
+
+#  print(pi_million)
+
+whereIsIt = pi_million.find(BIRTHDAY)
+
+if whereIsIt > 0:
+    print('yes @', whereIsIt)
+else:
+    print('no')
