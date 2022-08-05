81d80ac3d2af115c0f1fb5bb52e95b791dc6f130
David S Frech <dfrech@hotmail.com>
2022-08-05 16:53:33 -0400

    create todo for pyWordcount

--- pyWordcount.py
+++ pyWordcount.py
@@ -1,6 +1,6 @@
 """
     This program uses Sqlite3 to store the words in a book and how many
-    times the words are used.  The book is from https://gutenberg.org 
+    times the words are used.  The book is from https://gutenberg.org
     and stored as a text file in alice.txt.
 """
 
